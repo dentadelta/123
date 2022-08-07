0dbef384a8190bc147ddb6998fd942e6de84b5c7
delta <65842385+dentadelta@users.noreply.github.com>
2022-08-08 07:09:23 +1000

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -18,31 +18,31 @@ def optimize_ppo(trial):
         'gamma': trial.suggest_uniform('gamma', 0.8, 0.99999),
         'learning_rate': trial.suggest_uniform('learning_rate', 1e-5, 1e-3),
         'clip_range': trial.suggest_uniform('clip_range', 0.1, 0.4),
-        'gae_lambda': trial.suggest_uniform('gae_lambda', 0.8, 0.99)
+        'gae_lambda': trial.suggest_uniform('gae_lambda', 0.8, 0.99),
+        'max_grad_norm': trial.suggest_uniform('max_grad_norm', 0.1, 0.6),
+        'vf_coef': trial.suggest_uniform('vf_coef', 0.1, 0.9),
+        'ent_coef': trial.suggest_uniform('ent_coef', 0.0, 0.1),
+        'batch_size': trial.suggest_int('batch_size', 64, 256),
     }
 
 def optimize_agent(trial):
-    try:
-        model_parameter = optimize_ppo(trial)
-        env = gym.make('text_on_image-v0')
-        env= Monitor(env, "./tensorboard_log/")
-        env = make_vec_env(lambda: env, n_envs=12)
+
+    model_parameter = optimize_ppo(trial)
+    env = gym.make('text_on_image-v0')
+    env= Monitor(env, "./tensorboard_log/")
+    env = make_vec_env(lambda: env, n_envs=1)
 
 
-        model = PPO('MlpPolicy',env=env,tensorboard_log="./tensorboard_log/",verbose=0,**model_parameter)
-        model.learn(total_timesteps=2000000)
-        mean_reward,_ = evaluate_policy(model, env, n_eval_episodes=10)
-        env.close()
+    model = PPO('MlpPolicy',env=env,tensorboard_log="./tensorboard_log/",verbose=0,**model_parameter)
+    model.learn(total_timesteps=2000000, tb_log_name=f'Optuna_PPO_{trial.number}')
+    mean_reward,_ = evaluate_policy(model, env, n_eval_episodes=12)
+    env.close()
         
-        model.save("ppo_model{}".format(trial.number))
-        return mean_reward
-
-    except Exception as e:
-        return -1000
-
+    model.save("ppo_model{}".format(trial.number))
+    return mean_reward
 if __name__ == '__main__':
-    study = optuna.create_study(direction='maximize')
-    study.optimize(optimize_agent, n_trials=100, n_jobs=1)
+    study = optuna.create_study(direction='maximize',study_name='ppo_optimization')
+    study.optimize(optimize_agent, n_trials=200, n_jobs=10)
 
     env = gym.make('text_on_image-v0')
     use_model = False # True
