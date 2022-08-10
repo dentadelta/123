b95ff1796bebc36fddfeff152381243296034e81
delta <65842385+dentadelta@users.noreply.github.com>
2022-08-11 03:50:52 +1000

    Update __init__.py

--- deltagym/delta_gym/__init__.py
+++ deltagym/delta_gym/__init__.py
@@ -5,3 +5,5 @@ register(
     entry_point='delta_gym.envs:TextOnImageEnv',
     max_episode_steps=14,
 )
+
+register(id="deltaenv-v0", entry_point="delta.envs:StructureManagementEnv", max_episode_steps = 100)
