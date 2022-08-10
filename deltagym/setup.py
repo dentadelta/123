b2ee70e497250449f48f7f732bb4c808252d9898
delta <65842385+dentadelta@users.noreply.github.com>
2022-08-11 03:52:31 +1000

    Update setup.py

--- deltagym/setup.py
+++ deltagym/setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages()
 
-setup(name='placetextblockonimage',
+setup(name='delta_gym',
 version='0.0.1',
 install_required=['Pillow','pydantic', 'torch==1.12.1', 'gym==0.21.0', 'stable_baselines3'],
 description='This is a package for training a model to place text on an image without overlapping with other text',
 readme='README.md',
+packages=find_packages())    
 )
