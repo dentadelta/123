b0978a4d91b0ce83969bac4ce336c9f6596856a3
delta <65842385+dentadelta@users.noreply.github.com>
2022-10-03 23:52:18 +1000

    Update App.js

--- heavy_vehicle/App.js
+++ heavy_vehicle/App.js
@@ -1,17 +1,31 @@
 
 import './App.css';
 import { SearchButton } from './components/SearchButton';
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import { ExtractedData } from './components/ExtractedData';
-import axios from 'axios';
-import { Button, Typography} from "@mui/material";
+import { Typography} from "@mui/material";
 import { NavBar } from './components/NavBar';
-import { ProgressBar } from './components/ProgressBar';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import React from 'react'
+import {FastAPI} from './components/FastAPI';
 
 function App() {
   const [search, setSearch] = useState('');
   const [extractedData, setExtractedData] = useState({});
   const [showExtractedData, setShowExtractedData] = useState(true);
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const {response,loading} = FastAPI('/case', {caseNumber: search}, buttonClicked);
+  
+  const extractCaseButton = () => {
+    setShowExtractedData(false);
+    if (response) {
+      setButtonClicked(!buttonClicked);
+      setExtractedData(response.data);
+      setShowExtractedData(true);}
+    if (loading) {
+      setShowExtractedData(false);
+    }
+  };
 
   const handleSearch = event => {
     setSearch(event.target.value);
@@ -28,32 +42,13 @@ function App() {
   const handleUnfocus = () => {
     setShowExtractedData(true);
   };
-  const extractCaseButton = () => {
-    const caseNumber = search;
-    setSearch('');
-    setShowExtractedData(false);
-    if (caseNumber === '') {
-
-    } else {
-      axios.post('http://localhost:8000/case', {caseNumber: caseNumber})
-      .then(res => {
-        console.log(res.data.data);
-        setExtractedData(res.data.data);
-        setShowExtractedData(true);
-      })
-    }
-  }
-
-
-
-
-
-
+  
+  const clientID = process.env.REACT_APP_PAYPAL_CLIENT_ID;
+  
   return (
+    <PayPalScriptProvider options={{ "client-id": clientID }}>
     <div className="App">
-      <NavBar />
-      <br /><br /><br />
-      
+      <NavBar /><br /><br /><br />
       <header className="App-header">
         <br/>
       <Typography variant='h4' align='center'>Heavy Vehicle Case Extractor</Typography>
@@ -67,6 +62,7 @@ function App() {
       </header>
       
     </div>
+    </PayPalScriptProvider>
   );
 }
 
