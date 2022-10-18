c51ae3703d0e6a5c3c92ac7e7bd0ece78278041e
delta <65842385+dentadelta@users.noreply.github.com>
2022-10-19 03:34:46 +1000

    Create showimageintable.py

---
+++ showimageintable.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from IPython.core.display import HTML
+
+def callback(x,filename=None):
+    def path_to_image_html(path):
+        return '<img src="' + path + '" width="100" >'
+    x =x.to_html(escape=False,formatters=dict(photo=path_to_image_html))
+    if fileName is not None:
+      with open(fileName,'w') as file:
+        file.write(x)
+        file.close()
+    return HTML(x)
+  
+  
+  # df = pd.read_csv('...')
+  callback(df)
